7b973d30eb87814c43d142574c8eed77291b31eb
lilyei <53099752+lilyei@users.noreply.github.com>
2019-11-22 14:03:28 -0600

    Update manu.sql

--- manu.sql
+++ manu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-11-2019 a las 16:47:31
+-- Tiempo de generación: 22-11-2019 a las 21:01:46
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.2.15
 
@@ -31,11 +31,41 @@ SET time_zone = "+00:00";
 CREATE TABLE `actividad` (
   `idactividad` int(10) NOT NULL,
   `actividad` varchar(500) NOT NULL,
+  `proyecto` varchar(100) NOT NULL,
   `personal` varchar(500) NOT NULL,
   `horas` int(10) NOT NULL,
   `semana` int(10) NOT NULL,
-  `fecha` date NOT NULL
+  `fecha` date NOT NULL,
+  `observaciones` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `actividad`
+--
+
+INSERT INTO `actividad` (`idactividad`, `actividad`, `proyecto`, `personal`, `horas`, `semana`, `fecha`, `observaciones`) VALUES
+(1, 'asaas', 'Press Station Q5', 'Erick Monzon', 0, 0, '2019-11-22', ''),
+(2, 'wadad', '1410 Q5', 'Ana Karen Uribe ', 12, 0, '2019-11-16', 'no sabe');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nombre_interno`
+--
+
+CREATE TABLE `nombre_interno` (
+  `idinterno` int(11) NOT NULL,
+  `proyecto` varchar(100) NOT NULL,
+  `nombre_interno` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `nombre_interno`
+--
+
+INSERT INTO `nombre_interno` (`idinterno`, `proyecto`, `nombre_interno`) VALUES
+(1, 'AE-006-008 8440-TIGUAN', 'asas'),
+(2, 'AE-006-008 8440-TIGUAN', 'awdwadwa');
 
 -- --------------------------------------------------------
 
@@ -87,7 +117,12 @@ CREATE TABLE `proyectos` (
 
 INSERT INTO `proyectos` (`id_proyecto`, `proyecto`) VALUES
 (1, 'Press Station Q5'),
-(2, '1410 Q5');
+(2, '1410 Q5'),
+(4, 'aaaaaaaaaaaa'),
+(5, 'foko'),
+(6, ''),
+(7, 'LILYEI|'),
+(8, 'AE-006-008 8440-TIGUAN');
 
 --
 -- Índices para tablas volcadas
@@ -98,6 +133,12 @@ INSERT INTO `proyectos` (`id_proyecto`, `proyecto`) VALUES
 --
 ALTER TABLE `actividad`
   ADD PRIMARY KEY (`idactividad`);
+
+--
+-- Indices de la tabla `nombre_interno`
+--
+ALTER TABLE `nombre_interno`
+  ADD PRIMARY KEY (`idinterno`);
 
 --
 -- Indices de la tabla `personal`
@@ -119,7 +160,13 @@ ALTER TABLE `proyectos`
 -- AUTO_INCREMENT de la tabla `actividad`
 --
 ALTER TABLE `actividad`
-  MODIFY `idactividad` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `idactividad` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `nombre_interno`
+--
+ALTER TABLE `nombre_interno`
+  MODIFY `idinterno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `personal`
@@ -131,7 +178,7 @@ ALTER TABLE `personal`
 -- AUTO_INCREMENT de la tabla `proyectos`
 --
 ALTER TABLE `proyectos`
-  MODIFY `id_proyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_proyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
