46b8fd9eb699823b680eebf3aee97f78b5dc4d7f
Smart-AE <sistemas@smart-ae.com>
2019-11-26 08:19:13 -0600

    Create resultados.php

---
+++ resultados.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resultados</title>
+	<?php
+	include("conexion.php");
+	include("menu.php");
+
+
+	?>
+</head>
+<body>
+
+	<div>
+		<h1 align="center">Resultados</h1>
+		<div>
+			<table border="1" cellpadding="" align="center" class="table table-responsive">
+				<th>Proyecto</th>
+				<th>Semana</th>
+				<th>Total de Horas</th>
+			
+			</table>
+		</div>
+	</div>
+
+</body>
+</html>
