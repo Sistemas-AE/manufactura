35e2b7a5980cd477d9e22e3d1c0ce9ed5d73499c
Smart-AE <sistemas@smart-ae.com>
2019-11-26 13:26:13 -0600

    21

--- resultados.php
+++ resultados.php
@@ -3,25 +3,89 @@
 <head>
 	<title>Resultados</title>
 	<?php
+	error_reporting(0);
 	include("conexion.php");
 	include("menu.php");
+
+    $actividad = "SELECT * FROM `actividad`" ;
+    $oactividad =mysqli_query($conecta,$actividad)or die("no se realizo la consulta");
 
 
 	?>
 </head>
 <body>
-
-	<div>
+<form action="resultados.php" method="POST">
+	<div >
 		<h1 align="center">Resultados</h1>
-		<div>
-			<table border="1" cellpadding="" align="center" class="table table-responsive">
+		<div align="center">
+			<h2>Semana de Trabajo</h2>
+			<input type="date" name="fecha" id="fecha">
+		</div>
+		<br>
+
+		<div align="center">
+			<button type="submit" class="btn btn-info">Resultados</button>
+		</div>
+
+		<br>
+		<br>
+		<?php
+		$semana=" ";
+
+		$semana=$_POST['fecha'];
+		
+		$semana = date("W",strtotime($semana)); 
+		echo $semana;
+		?>
+
+		<div class="d-flex justify-content-center" align="center">
+			<table class="table table-striped" border="1" cellpadding="10" style="width: 40%">
+			<div>
+				<tr>
 				<th>Proyecto</th>
+				<th>Horas</th>
 				<th>Semana</th>
-				<th>Total de Horas</th>
+				</tr>
+			</div>
+			<div>
+				<?php
+					while ($aactividad=mysqli_fetch_array($oactividad)) {
+                    ?>
+				<tr>
+					
+                    <th>
+                    	 <?php
+                        echo $aactividad['proyecto'];
+                        ?>
+                    </th>
+                    <th>
+                    	<?php
+                        echo $aactividad['horas'];
+                        ?>
+                    </th>
+                     <th>
+                    	<?php
+                        echo $aactividad['semana'];
+                        ?>
+                    </th>
+
+                    <?php
+                    }
+                    ?>
+				</tr>
+			</div>
 			
+
+
+
+
+
+
+			
+				
 			</table>
 		</div>
 	</div>
-
+</form>
 </body>
 </html>
